87319c2b551342b91ed7d528841b9f1b92e3b4d5
8bit <139103755+RGBXD4@users.noreply.github.com>
2025-01-25 17:48:00 +0800

    Update Paths.hx

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -469,7 +469,7 @@ class Paths
 
 	#if MODS_ALLOWED
 	inline static public function mods(key:String = '') {
-		return 'mods/' + key;
+		return Sys.getCwd() + 'mods/' + key;
 	}
 
 	inline static public function modsFont(key:String) {
@@ -531,7 +531,7 @@ class Paths
 			if(FileSystem.exists(fileToCheck))
 				return fileToCheck;
 		}
-		return 'mods/' + key;
+		return Sys.getCwd() + 'mods/' + key;
 	}
 	#end
 
